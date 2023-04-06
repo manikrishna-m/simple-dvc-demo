7860e07e8e01fd7dffda637d14dd1e7b1e702225
Mani Krishna <mandepudi.mk@gmail.com>
2023-04-06 19:35:31 +0100

    added steup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "src",
+    version = "0.0.1",
+    description = "End to End Implementation of MLOps project",
+    author = "Mani Krishna Mandepudi",
+    packages = find_packages(),
+    license = "MIT", 
+)
